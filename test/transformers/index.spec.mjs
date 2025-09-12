6ed328f3aaf40d34ff51a06f14a2c4444f97acf4
Jonathan Perry <jonathanperry@modernpoacher.com>
2025-09-12 21:11:58 +0100

    `a('module')`

--- test/transformers/index.spec.mjs
+++ test/transformers/index.spec.mjs
@@ -13,28 +13,28 @@ describe('#sprockets/transformers', () => {
   describe('`checkAnswers`', () => {
     it('is an object', () => {
       expect(checkAnswers)
-        .to.be.an('object')
+        .to.be.a('module') // an('object')
     })
   })
 
   describe('`common`', () => {
     it('is an object', () => {
       expect(common)
-        .to.be.an('object')
+        .to.be.a('module') // an('object')
     })
   })
 
   describe('`errorMessage`', () => {
     it('is an object', () => {
       expect(errorMessage)
-        .to.be.an('object')
+        .to.be.a('module') // an('object')
     })
   })
 
   describe('`errorSummary`', () => {
     it('is an object', () => {
       expect(errorSummary)
-        .to.be.an('object')
+        .to.be.a('module') // an('object')
     })
   })
 })
